46105e4836ab15fb6b83d0da39b180595ad4b53c
Will Stockhauser <stockhw@sunypoly.edu>
2024-11-10 01:28:23 -0500

    bug fixes

--- MERN_PROJECT/Backend/Routes/movieAPIRoutes.js
+++ MERN_PROJECT/Backend/Routes/movieAPIRoutes.js
@@ -1,38 +1,21 @@
 import express from 'express';
-import Movie from '../Models/movieModel.js';
+import { getMovies, getMovie, createMovie, deleteMovie, updateMovie } from '../Controllers/movieController'
 
 const router = express.Router();
 
-// GET all movies:
-router.get('/', (req, res) => {
-    res.json({ mssg: 'GET all movies' });
-});
+// GET all movies
+router.get('/', getMovies);
 
-// GET a single movie:
-router.get('/:id', (req, res) => {
-    res.json({ mssg: 'GET a single movie' });
-});
+// GET a single movie
+router.get('/:id', getMovie);
 
-// POST a new movie:
-router.post('/', async (req, res) => {
-    const { title, genre, releaseDate, rating, duration } = req.body;
+// POST a new movie
+router.post('/', createMovie);
 
-    try {
-        const movie = await Movie.create({ title, genre, releaseDate, rating, duration });
-        res.status(200).json(movie);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+// DELETE a movie
+router.delete('/:id', deleteMovie);
 
-// DELETE a movie:
-router.delete('/:id', (req, res) => {
-    res.json({ mssg: 'DELETE a movie' });
-});
-
-// UPDATE a movie:
-router.patch('/:id', (req, res) => {
-    res.json({ mssg: 'UPDATE a movie' });
-});
+// UPDATE a movie
+router.patch('/:id', updateMovie);
 
 export default router;
